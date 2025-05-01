7a64fab47bd3e62a98a3e5271bcb05b1e47aee78
music-miko <voilet856@gmail.com>
2025-05-02 03:12:45 +0530

    Update bot_session_gen.js

--- bot_session_gen.js
+++ bot_session_gen.js
@@ -2,27 +2,31 @@ const { TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions/index.js");
 const input = require("input"); // npm install input
 
-const apiId = parseInt(await input.text("Enter your API ID: "));
-const apiHash = await input.text("Enter your API Hash: ");
-const botToken = await input.text("Enter your Bot Token: ");
-const stringSession = new StringSession(""); // Empty session
+async function generateSession() {
+    const apiId = parseInt(await input.text("Enter your API ID: "));
+    const apiHash = await input.text("Enter your API Hash: ");
+    const botToken = await input.text("Enter your Bot Token: ");
+    const stringSession = new StringSession(""); // Empty session
 
-console.log("\nGenerating session...");
+    console.log("\nGenerating session...");
 
-const client = new TelegramClient(stringSession, apiId, apiHash, {
-    connectionRetries: 5,
-});
-
-try {
-    await client.start({
-        botAuthToken: botToken,
+    const client = new TelegramClient(stringSession, apiId, apiHash, {
+        connectionRetries: 5,
     });
 
-    console.log("\n== Your Bot String Session ==");
-    console.log(client.session.save());
-    console.log("\nKeep this string safe.");
-    process.exit(0);
-} catch (error) {
-    console.error("\nError:", error.message || error);
-    process.exit(1);
+    try {
+        await client.start({
+            botAuthToken: botToken,
+        });
+
+        console.log("\n== Your Bot String Session ==");
+        console.log(client.session.save());
+        console.log("\nKeep this string safe.");
+        process.exit(0);
+    } catch (error) {
+        console.error("\nError:", error.message || error);
+        process.exit(1);
+    }
 }
+
+generateSession();
